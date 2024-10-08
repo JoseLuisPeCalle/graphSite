9463233571b081968f47b494201e545c717082c9
JoseLuisPCalle <joseluis.perezcalle@gm.uca.es>
2024-10-08 14:02:16 +0200

    Update

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ function showSection(sectionId) {
         sectionToShow.classList.add('is-visible');
     }, 100);
 }
+function toggleGraphs() {
+    var graphSection = document.getElementById("graphSection");
+    var button = document.querySelector(".toggle-button"); // Selecciona el botón
+    if (graphSection.style.display === "none" || graphSection.style.display === "") {
+        graphSection.style.display = "flex"; // Muestra el bloque de gráficos
+        button.textContent = "Hide Plot"; // Cambia el texto del botón
+    } else {
+        graphSection.style.display = "none"; // Oculta el bloque de gráficos
+        button.textContent = "Show Plot"; // Cambia el texto del botón
+    }
+}
+
+// Oculta el bloque de gráficos por defecto al cargar la página
+window.onload = function() {
+    var graphSection = document.getElementById("graphSection");
+    graphSection.style.display = "none"; // Asegúrate de que los gráficos estén ocultos al inicio
+};
