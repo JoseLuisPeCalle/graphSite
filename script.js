94ecbbf8225c8a177b5070b185a3d8c941a6408a
JoseLuisPCalle <joseluis.perezcalle@gm.uca.es>
2024-10-09 17:25:37 +0200

    Update

--- script.js
+++ script.js
@@ -19,20 +19,31 @@ function showSection(sectionId) {
         sectionToShow.classList.add('is-visible');
     }, 100);
 }
-function toggleGraphs() {
-    var graphSection = document.getElementById("graphSection");
-    var button = document.querySelector(".toggle-button"); // Selecciona el botón
+
+function toggleGraphs(sectionId, buttonId) {
+    var graphSection = document.getElementById(sectionId);
+    var button = document.getElementById(buttonId);
+
+    // Verificar si los gráficos están ocultos
     if (graphSection.style.display === "none" || graphSection.style.display === "") {
-        graphSection.style.display = "flex"; // Muestra el bloque de gráficos
-        button.textContent = "Hide Plot"; // Cambia el texto del botón
+        graphSection.style.display = "block"; // Muestra el bloque de gráficos
+        button.textContent = buttonId === 'toggleButtonPCA' ? "Hide PCA Plots" : buttonId === 'toggleButtonImages' ? "Hide Dendograms" : "Hide Plots"; // Cambia el texto del botón
     } else {
         graphSection.style.display = "none"; // Oculta el bloque de gráficos
-        button.textContent = "Show Plot"; // Cambia el texto del botón
+        button.textContent = buttonId === 'toggleButtonPCA' ? "Show PCA Plots" : buttonId === 'toggleButtonImages' ? "Show Dendograms" : "Show Plots"; // Cambia el texto del botón
     }
 }
 
-// Oculta el bloque de gráficos por defecto al cargar la página
+// Ocultar gráficos por defecto al cargar la página
 window.onload = function() {
-    var graphSection = document.getElementById("graphSection");
-    graphSection.style.display = "none"; // Asegúrate de que los gráficos estén ocultos al inicio
+    var imageGraphSection = document.getElementById("graphSectionIframes");
+    var pcaGraphSection = document.getElementById("graphSection2");
+    var imgGraphSection = document.getElementById("graphSectionImages");
+
+    // Asegúrate de que los gráficos estén ocultos al inicio
+    imageGraphSection.style.display = "none"; 
+    pcaGraphSection.style.display = "none"; 
+    imgGraphSection.style.display = "none"; // Oculta la sección de imágenes
 };
+
+
